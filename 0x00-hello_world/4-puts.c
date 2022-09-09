e2d8fe712d1520f83944c5e87f746ac7d2a39fb9
emeka prince ijeh <prinlam7@gmail.com>
2022-09-08 20:20:17 -0700

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 
+
 /**
  * main - Printing
  *
  * Return: Always 0 (Successful)
  */
+
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
 
