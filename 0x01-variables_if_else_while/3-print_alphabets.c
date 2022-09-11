343f49aef65e302995a0af29ca73e9c3d49ac277
emeka prince ijeh <prinlam7@gmail.com>
2022-09-11 15:28:27 -0700

    commited

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ * and uppercase
+ * Return: Always 0 (successful)
+ */
+
+int main(void);
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
