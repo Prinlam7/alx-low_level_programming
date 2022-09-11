003e7800e241777c155940c470e9176c13dc394b
emeka prince ijeh <prinlam7@gmail.com>
2022-09-11 16:04:47 -0700

    commited

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 10 
+ * starting from 0
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	int n = 0;
+	while (n <= 10)
+	{
+		printf("%d", n);
+	}
+	n++
+	printf("\n");
+	return (0);
+}
