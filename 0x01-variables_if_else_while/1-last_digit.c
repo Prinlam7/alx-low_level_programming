2c0dab06594423cc4c0e795ce054f07e851da7ef
emeka prince ijeh <prinlam7@gmail.com>
2022-09-11 10:09:20 -0700

    commited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int n;
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d ", n, n % 10);
@@ -20,5 +19,5 @@ int main(void)
 		printf("and is 0\n");
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
-	return 0;
+	return (0);
 }
