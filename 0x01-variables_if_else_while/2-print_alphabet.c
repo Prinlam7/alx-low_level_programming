0c5edfab1ba833f1248d5dc86cfe2727288c511c
emeka prince ijeh <prinlam7@gmail.com>
2022-09-11 15:17:04 -0700

    commited

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - print alphabet in lowercase 
@@ -10,10 +10,10 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++);
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
