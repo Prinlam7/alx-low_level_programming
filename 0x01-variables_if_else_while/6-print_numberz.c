27577094330382441093bb5352c37542cf4a2bd8
emeka prince ijeh <prinlam7@gmail.com>
2022-09-11 17:52:18 -0700

    commited

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit 
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n')
+	return (0);
+}
