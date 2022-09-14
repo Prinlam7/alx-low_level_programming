40d988abe0ce34484a8c6b28756d660c53dca78f
emeka prince ijeh <prinlam7@gmail.com>
2022-09-14 09:08:59 -0700

    commited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lowercase 
+ *
+ * Return: Always 0 (successful)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i >= 'z'; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n')
+	
+	return (0);
+}
