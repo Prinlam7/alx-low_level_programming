f86e3af880d4bf8eae61d7e457ba3c81862ed478
emeka prince ijeh <prinlam7@gmail.com>
2022-09-14 21:41:05 -0700

    commited

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print last digit
+ *
+ * Return: Always 0 
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+	n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
+}
