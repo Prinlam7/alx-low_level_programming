1a1abc25d9c04f52f77ea97f048d219f143fe8f4
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 05:02:15 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; 1++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+	_putchar('\n');
+}
