69b9379fbcc85abdef96ed4a7c4b250e91380399
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 05:44:34 -0700

    commited

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - draw a straight line 
+ * print_line - draw a straight line
  * @n: number of lines
  * Return: 0
  */
@@ -9,12 +9,11 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	}else
 	{
 		int i;
 
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar('_');
 		}
