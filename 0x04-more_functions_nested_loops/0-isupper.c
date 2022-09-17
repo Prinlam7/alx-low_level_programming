dd1c72eace73b1952fb83269ed45676f90f5f9ee
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 04:00:21 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase characters
+ * @c : the character to be checked
+ * REturn: 0 
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
