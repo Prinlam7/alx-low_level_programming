f56b3b8ac2eaef7b8fe8a8aa1ee765b6d88fcea9
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 04:17:40 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two intergals
+ * @a: to be multiplied by b
+ * @b: to be multiplied by a
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
