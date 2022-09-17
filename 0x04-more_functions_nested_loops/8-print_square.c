5adee465d0679167e54001e987f0888ff370a3a7
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 06:24:41 -0700

    commited

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_square - print square 
+ * print_square - print square
  * @size: size of square
  */
 void print_square(int size)
@@ -17,7 +17,7 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar(#);
+				_putchar('#');
 			}
 			_putchar('\n');
 		}
