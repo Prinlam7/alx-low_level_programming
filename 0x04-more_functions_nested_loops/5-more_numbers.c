bdb3d76cc37b500a2d13d585b6e752ff9c22b57f
emeka prince ijeh <prinlam7@gmail.com>
2022-09-17 05:08:34 -0700

    commited

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,8 @@
 #include "main.h"
 /**
+ * more_numbers - print numbers
+ *
+ * return: 0
  */
 void more_numbers(void)
 {
@@ -10,7 +13,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
 		}
 	}
 	_putchar('\n');
