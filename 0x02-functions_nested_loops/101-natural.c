75a491a176823f26dc083272acc847189041c786
emeka prince ijeh <prinlam7@gmail.com>
2022-09-18 21:14:12 -0700

    commited

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print sum of multiples
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	
+	for (i = 0; 1 < 1024; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			a = a + i;
+		}
+		else if ((i % 5) == 0)
+		{
+			b = b + i;
+		}
+	}
+	c = a + b;
+	printf("%iu\n", c);
+
+	return (0);
+}
