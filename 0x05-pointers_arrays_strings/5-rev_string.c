86be44d8c3d018dfd1bd193d259c46385cefdd2f
emeka prince ijeh <prinlam7@gmail.com>
2022-09-21 03:15:50 -0700

    commited

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s:the string
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len1;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	len1 = len - 1
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
+}
