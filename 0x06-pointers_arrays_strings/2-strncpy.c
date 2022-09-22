be6be537a3620dfd1c475f78078b47471382da91
emeka prince ijeh <prinlam7@gmail.com>
2022-09-22 06:55:07 -0700

    commited

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**strncpy - copie a string
+ * @dest: destination string
+ * @src: sourse string
+ * @n: number of bytes
+ * Return: pointer 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
