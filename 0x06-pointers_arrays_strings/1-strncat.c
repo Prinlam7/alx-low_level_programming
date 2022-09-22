d2802e1515ca4b3676bee3bc242308467b460c8a
emeka prince ijeh <prinlam7@gmail.com>
2022-09-22 06:30:14 -0700

    commited

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - concatenate n bytes
+ * @dest: destination strings
+ * @src: source string
+ * @n: number of bytes or src to concaternate
+ * Return: a pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (desr[i] != '\0')
+	{
+		i++;
+	}
+	while(src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
