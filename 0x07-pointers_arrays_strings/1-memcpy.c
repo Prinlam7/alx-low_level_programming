1abe4ef36591e008d43d77c6ca2adf6c1e1ddd59
emeka prince ijeh <prinlam7@gmail.com>
2022-09-27 06:01:34 -0700

    commiyrd

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * @dest: destination memory area
  * @src:mem to copy from
  * @n: number of byte to copy
- * 
+ *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
