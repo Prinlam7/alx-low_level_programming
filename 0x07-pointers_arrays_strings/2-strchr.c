d887dc32cfe35cd3e833a965709c7c014cc6b00e
emeka prince ijeh <prinlam7@gmail.com>
2022-09-27 06:11:20 -0700

    commiyed

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strchr - locates a char in a string
+ * @: string
+ * @c: char
+ *
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
