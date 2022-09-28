cebc837bec61cf2d55f3953fdc74a11c872b8348
emeka prince ijeh <prinlam7@gmail.com>
2022-09-28 01:21:48 -0700

    commited

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * set_string - sets the value of a pointer to char
- * @s: pointer to pointer we need to set to 
+ * @s: pointer to pointer we need to set to
  * @to: string to set
  */
 void set_string(char **s, char *to)
