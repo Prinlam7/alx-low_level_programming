3c4bade89e2d3e4205e9b4cba003bc0cdf8db2c2
emeka prince ijeh <prinlam7@gmail.com>
2022-09-29 04:56:14 -0700

    commited

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursions(s + 1);
+}
