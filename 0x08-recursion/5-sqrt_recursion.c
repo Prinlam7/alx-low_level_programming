4af3ff363e24317029daeb9c147ba7775eb8cac5
emeka prince ijeh <prinlam7@gmail.com>
2022-09-29 06:32:08 -0700

    commited

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,6 +4,7 @@ int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_rescursion - returns the natural square root of a number
+ * actual_sqrt_recursion - recurse to find the natural square of a number
  * @n: number to calculate the square root of
  *
  * Return: the resulting square root
