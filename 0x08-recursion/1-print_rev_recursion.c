02dbd8ae55ba7873b9c610897e37ca3fe839ee8d
emeka prince ijeh <prinlam7@gmail.com>
2022-09-29 05:19:32 -0700

    commited

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _print_rev_recursion - print a string in reverse 
+ * _print_rev_recursion - print a string in reverse
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
