dd0f32825ea433de44442ed2855f8cb99533cb24
emeka prince ijeh <prinlam7@gmail.com>
2022-10-03 09:14:12 -0700

    commited

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the number arguments  passed
+ * @argc: numbers
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
