817b4e09231dbad707e3d60bef598fe5d06c373f
emeka prince ijeh <prinlam7@gmail.com>
2022-10-03 08:52:45 -0700

    commited

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 /**
+ * main - print the name of the program
+ * @argc: number of argument
+ * @argv: array of arguments
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
