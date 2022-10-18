df438cf83ba2beafbab544a456488694e0ee0586
emeka prince ijeh <prinlam7@gmail.com>
2022-10-18 01:32:06 -0700

    commited

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -41,8 +41,8 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
- * new_dog - creates a new dog@
- * name: name of the dog
+ * new_dog - creates a new dog
+ * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  * Return: pointer to the new dog (Success), NULL otherwise
